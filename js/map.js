f5dc1d8eac0fb76caf5b4a87b13903e673d88fbb
vmo2np360-cpu <vmo2.np360@gmail.com>
2026-09-02 10:43:19 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -54,7 +54,7 @@ async function mapInit() {
     if (defs) mapSvg.appendChild(defs);
 
     // 設定 viewBox（固定）
-    mapSvg.setAttribute('viewBox', '0 100 2800 600');
+    mapSvg.setAttribute('viewBox', '0 150 2800 550');
 
     // ★ 從 Firestore 讀取偏移量
     mapGlobalOffset = await window.getGlobalOffsetFromFirestore();
